65f6656a258fd297545eba9cacf034dbb0462e9d
Adam <jljastiven13@gmail.com>
2026-05-03 17:23:30 +0800

    Adapted to changes in global components

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import tkinter as tk
+import customtkinter as ctk
 from ui.login_ui import LoginUI
-   
+
+
 def main():
-    root = tk.Tk()
-    app = LoginUI(root)
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
+
+    root = ctk.CTk()
+    LoginUI(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
